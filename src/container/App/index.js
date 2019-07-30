14bc7f07db2358df6d96e7c8524c7e182ec4fc00
Alia <41635870+ahaidar98@users.noreply.github.com>
2019-07-29 22:39:44 -0500

    Update index.js

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -1,48 +1,48 @@
 import 'regenerator-runtime/runtime';
-	import React from 'react';
-	import { Provider } from 'react-redux';
-	import { createStore, applyMiddleware, compose } from 'redux';
-	import { routerReducer, routerMiddleware } from 'react-router-redux';
-	import { createBrowserHistory as createHistory } from 'history'
-	import createSagaMiddleware from 'redux-saga';
-	import reduxReset from 'redux-reset';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import { createBrowserHistory as createHistory } from 'history'
+import createSagaMiddleware from 'redux-saga';
+import reduxReset from 'redux-reset';
 
-	import routes from '../../routes';
-	import createReducers from '../../reducers';
-	import rootSaga from '../../sagas';
-	import './styles.css';
+import routes from '../../routes';
+import createReducers from '../../reducers';
+import rootSaga from '../../sagas';
+import './styles.css';
 
-	const composeEnhancers = typeof window === 'object' &&
-	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-	  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+const composeEnhancers = typeof window === 'object' &&
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-	const App = () => {
-	  // store is created with the reducers that are in /reducers
+const App = () => {
+  // store is created with the reducers that are in /reducers
 
-	  const reducers = createReducers(routerReducer);
-	  const sagaMiddleware = createSagaMiddleware();
-	  const history = createHistory();
+  const reducers = createReducers(routerReducer);
+  const sagaMiddleware = createSagaMiddleware();
+  const history = createHistory();
 
-	  history.listen((loc, act) => {
-	    const container = window.document.getElementById('mainContainer');
+  history.listen((loc, act) => {
+    const container = window.document.getElementById('mainContainer');
 
-	    if (container) container.scrollTop = 0;
-	  });
+    if (container) container.scrollTop = 0;
+  });
 
-	  const middlewares = [sagaMiddleware, routerMiddleware(history)];
-	  const store = createStore(reducers, composeEnhancers(applyMiddleware(...middlewares), reduxReset()));
+  const middlewares = [sagaMiddleware, routerMiddleware(history)];
+  const store = createStore(reducers, composeEnhancers(applyMiddleware(...middlewares), reduxReset()));
 
-	  sagaMiddleware.run(rootSaga);
+  sagaMiddleware.run(rootSaga);
 
-	  // browser history sync with redux store
+  // browser history sync with redux store
 
-	  return (
-	    <Provider store={store}>
-	      <div id="mainContainer">
-	        {routes(history)}
-	      </div>
-	    </Provider>
-	  );
-	};
+  return (
+    <Provider store={store}>
+      <div id="mainContainer">
+	{routes(history)}
+      </div>
+    </Provider>
+  );
+};
 
-	export default App;
+export default App;
